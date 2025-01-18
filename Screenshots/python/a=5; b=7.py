82a58a7eecc0c55a6cb17169a2f97b8fe02f1185
PhuongHin-2005 <nguyenthiphuonghien05@gmail.com>
2025-01-19 00:11:52 +0700

    Delete Screenshots directory

--- Screenshots/python/a=5; b=7.py
+++
@@ -1,3 +0,0 @@
-a=5; b=7
-print("giá tri cua a va b la:")
-print(a, b,sep=',',end='\n')
--- Screenshots/python/nhạp và tổng bình phương.py
+++
@@ -1,7 +0,0 @@
-#nhập hai số nguyên
-so1 = int(input("nhập số thứ nhất: "))
-so2 = int(input("nhập số thứ hai: "))
-#Tính tổng bình phương
-tong_binh_phuong = so1**2 + so2**2
-#in tổng bình phương
-print("Tổng bình phương của", so1, "và", so2, "là", tong_binh_phuong)
